ae8d87aed9bd92a602ecba95be58ff656cc50afa
Fiction <spmsupun@gmail.com>
2017-12-01 14:40:49 +0530

    First test

---
+++ index.js
@@ -0,0 +1,26 @@
+let store = require('store');
+let request = require('request');
+
+class Igiya {
+
+    constructor() {
+
+    }
+
+    initialize(url,param = [], callback = function () {}) {
+
+        this.url = url;
+        this.param = url;
+
+        let self = this;
+
+        request
+            .get(self.url, function(error, response, body) {
+                callback(error, response, body);
+            });
+    }
+
+}
+
+
+module.exports = new Igiya();
