ac88e81e75c2bfa2a8abc2011a95e820de609469
Fiction <spmsupun@gmail.com>
2017-12-01 17:13:13 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 let store = require('store');
 let request = require('request');
+let _ = require('lodash');
 
 class Igiya {
 
     constructor() {
 
     }
-    initialize(url,param = [], callback = function () {}) {
+
+    initialize(url, param = [], store_name = 'igiya', callback = function () {
+    }) {
 
         this.url = url;
         this.param = url;
+        this.store_name = store_name;
 
         let self = this;
+        this.data = store.get(self.store_name);
+
+        if (this.initialized && this.data) {
+            callback(false, true, []);
+            return true;
+        }
 
         request
-            .get(self.url, function(error, response, body) {
-                store.set(Igiya,body);
-                callback(error, response, body);
+            .get(self.url, function (error, response, body) {
+
+                self.data = JSON.parse(body);
+
+                self.initialized = true;
+                store.set(self.store_name, self.data);
+                callback(error, response, self.data);
+                return true;
             });
+    }
+
+    search(attribute, keyword , matches = false) {
+
+        let self = this;
+        let data = store.get(self.store_name);
+
+        let filter_list = data;
+
+         if(matches)
+            filter_list = _.filter(filter_list, _.matches(matches));
+
+         if(keyword) {
+             filter_list = _.filter(filter_list, function (data_array) {
+                 return data_array[attribute].toUpperCase().includes(keyword.toUpperCase());
+             });
+         }
+
+        return filter_list;
+
     }
 
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
-'use strict';
-
-let expect = require('chai').expect;
+let chai = require("chai");
+let expect = chai.expect;
+let assert = chai.assert;
+chai.config.includeStack = true;
 let igiya = require('../index');
 
-igiya.initialize("http://apple.dev/ajax/map/search?type=place",{},function (error, response, body) {
-    console.log(body);
+it('should respond with redirect on get', function(done) {
+
+    this.timeout(5000);
+
+        igiya.initialize("http://apple.dev/ajax/map/search?type=place", {}, 'igiya', function (error, response, body) {
+
+            let result = igiya.search('name', null,{
+                'is_stop': true,
+            });
+
+            expect(result).to.be.an('array');
+            console.log(result);
+            done();
+
+        });
+
+
 });
+
+
